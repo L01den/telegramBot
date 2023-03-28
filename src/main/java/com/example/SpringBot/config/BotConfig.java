82276215d6379a79bf873012baebfa0f0db24995
Daria <procionestrisce@yandex.ru>
2023-03-28 22:31:41 +0300

    init Commit

--- src/main/java/com/example/SpringBot/config/BotConfig.java
+++ src/main/java/com/example/SpringBot/config/BotConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+
 @Configuration
 @Data
 @PropertySource("application.properties")
